e8a28bf83a31253c8e382457e25b0dd90ac665af
teigorme <igorteixeira354@gmail.com>
2025-05-21 19:39:56 +0100

    feat: initial setup for project

---
+++ src/server.ts
@@ -0,0 +1,53 @@
+import { fastify } from "fastify";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastifyCors } from "@fastify/cors";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { routes } from "./users/routes.js";
+
+const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Fastify API",
+      description: "Sample backend service",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/api/docs",
+});
+
+app.register(routes);
+
+async function run() {
+  try {
+    await app.ready();
+    await app.listen({
+      port: 3333,
+      host: "0.0.0.0",
+    });
+    app.log.info(`Server listening on http://localhost:3333`);
+    app.log.info(`Swagger UI available on http://localhost:3333/api/docs`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
+
+run();
---
+++ src/users/routes.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+import { FastifyTypeInstance } from "../@types/fastify-type-instance.js";
+
+export async function routes(router: FastifyTypeInstance) {
+  router.get(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "List users",
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string().uuid(),
+              name: z.string(),
+              email: z.string().email(),
+            })
+          ),
+        },
+      },
+    },
+    async () => {
+      return [];
+    }
+  );
+
+
+  router.post(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "Create a new user",
+        body: z.object({
+          name: z.string().min(3),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            id: z.string().uuid(),
+            name: z.string(),
+            email: z.string().email(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+
+      const newUser = {
+        id: "123e4567-e89b-12d3-a456-426614174000",
+        name,
+        email,
+      };
+
+      return reply.status(201).send(newUser);
+    }
+  );
+}
